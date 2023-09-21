18c2d93ded06b2a85fa8ee871ebc2e64a61f9220
mahdi_turrkk <mahdichavoshi14@gmail.com>
2023-09-21 14:00:01 +0330

    subscription lookup dialog connected to api

--- Frontend/src/components/subLookupDialog.vue
+++ Frontend/src/components/subLookupDialog.vue
@@ -9,7 +9,7 @@
       </div>
       <div class="font-bold text-sm text-center mb-4">{{ local.subLookUp }}</div>
       <div class="flex relative w-full">
-        <input type="text" :placeholder="local.subscriptionUrl"
+        <input type="text" :placeholder="local.subscriptionUrl" v-model="subLink"
                class="w-full shadow-lg mb-4 rounded-xl py-2 bg-background-2 text-info-3 placeholder:text-info-2 outline-none border-background-2 border-2 focus:border-primary-1 transition-all duration-150"
                :class="{'pr-4 pl-8' : isRtl , 'pl-4 pr-8' : !isRtl}" @keydown.enter="searchSubscription"
         />
@@ -18,7 +18,7 @@
                                @click="searchSubscription"/>
       </div>
       <div class="font-bold text-center flex flex-col space-y-2 px-2 relative text-sm"
-           v-if="lookupSubscription.id != undefined">
+           v-if="showSubDetail">
         <div class="flex">{{ local.id }}&nbsp;:&nbsp;<div class="font-normal"
                                                           :class="{'mr-auto' : isRtl , 'ml-auto' : !isRtl}">
           {{ lookupSubscription.id }}
@@ -31,32 +31,32 @@
         </div>
         <div class="flex">{{ local.startDate }}&nbsp;:&nbsp;<div class="font-normal"
                                                                  :class="{'mr-auto' : isRtl , 'ml-auto' : !isRtl}">
-          {{ lookupSubscription.startDate }}
+          {{ lookupSubscription.startDate ? lookupSubscription.startDate.substring(0,10) : '' }}
         </div>
         </div>
         <div class="flex">{{ local.expireDate }}&nbsp;:&nbsp;<div class="font-normal"
                                                                   :class="{'mr-auto' : isRtl , 'ml-auto' : !isRtl}">
-          {{ lookupSubscription.expireDate }}
+          {{ lookupSubscription.expireDate ? lookupSubscription.expireDate.substring(0,10) : '' }}
         </div>
         </div>
         <div class="flex">{{ local.totalUsed }}&nbsp;:&nbsp;<div class="font-normal" style="direction: ltr"
                                                                  :class="{'mr-auto' : isRtl , 'ml-auto' : !isRtl}">
-          {{ (Number(lookupSubscription.totalUsed) / Math.pow(1024, 3)).toFixed(1) }}&nbsp;&nbsp;&nbsp;GB
+          {{ lookupSubscription.totalUsed ? lookupSubscription.totalUsed : '0.00'}}&nbsp;&nbsp;&nbsp;GB
         </div>
         </div>
         <div class="flex">{{ local.totalFlow }}&nbsp;:&nbsp;<div class="font-normal" style="direction: ltr"
                                                                  :class="{'mr-auto' : isRtl , 'ml-auto' : !isRtl}">
-          {{ (Number(lookupSubscription.totalFlow) / Math.pow(1024, 3)).toFixed(1) }}&nbsp;&nbsp;&nbsp;GB
+          {{ lookupSubscription.totalFlow ? lookupSubscription.totalFlow : '0.00'}}&nbsp;&nbsp;&nbsp;GB
         </div>
         </div>
         <div class="flex">{{ local.status }}&nbsp;:&nbsp;<div class="font-normal"
                                                               :class="{'mr-auto' : isRtl , 'ml-auto': !isRtl}">
-          {{ lookupSubscription.status }}
+          {{ lookupSubscription.status ? local.active : local.inactive }}
         </div>
         </div>
       </div>
       <div class="text-info-2 px-6 md:px-10 pb-14"
-           v-if="lookupSubscription.id === undefined">
+           v-if="!showSubDetail">
         <document-magnifying-glass-icon class="w-36 h-36 mx-auto"/>
         <div class="text-xs md:text-sm">{{ local.enterSubToSearch }}</div>
       </div>
@@ -69,6 +69,8 @@ import {computed, reactive, ref, watch} from "vue";
 import {useLocalization} from "../store/localizationStore.js";
 import {XMarkIcon} from "@heroicons/vue/24/solid/index.js";
 import {DocumentMagnifyingGlassIcon, MagnifyingGlassIcon} from "@heroicons/vue/24/outline/index.js";
+import axios from "axios";
+import {useDataStore} from "../store/dataStore.js";
 
 let local = computed(() => {
   return useLocalization().getLocal
@@ -85,7 +87,23 @@ let lookupSubscription = reactive({
   status: true
 })
 
-const searchSubscription = ()=>{}
+let subLink = ref('')
+let showSubDetail = ref(false)
+
+const searchSubscription = ()=>{
+  if(subLink.value){
+    axios.get(`${useDataStore().getServerAddress}/subscriptions/report?subLink=${subLink.value}` ,
+        {
+          headers : {
+            Authorization : useDataStore().getToken
+          }
+        }
+    ).then((response) => {
+      lookupSubscription = response.data
+      showSubDetail.value = true
+    }).catch((error) => console.log(error))
+  }
+}
 
 
 const props = defineProps(['showDialog'])
