3166eac18371fb73c64b12b2a9389573eaa170b1
mahdi_turrkk <mahdichavoshi14@gmail.com>
2023-09-21 14:02:28 +0330

    all pages and components connected to api

--- Frontend/src/views/admin/servers/index.vue
+++ Frontend/src/views/admin/servers/index.vue
@@ -4,35 +4,43 @@
     <div class="w-full absolute top-5">
       <div class="w-fit bg-error text-white flex rounded-xl px-2 py-2 mx-auto items-center" v-if="showErrorMessage">
         <x-circle-icon class="w-5 h-5"/>
-        <div>{{ local.inboundsLoadFailed }}</div>
+        <div>{{ errorMessage }}</div>
       </div>
       <div class="w-fit bg-success text-white flex rounded-xl px-2 py-2 mx-auto items-center" v-if="showSuccessMessage">
         <check-circle-icon class="w-5 h-5"/>
-        <div>{{ local.inboundsLoadSuccessfully }}</div>
+        <div>{{ successMessage }}</div>
       </div>
     </div>
-    <div class=" rounded-xl w-full py-3 px-4 flex justify-between items-center">
+    <div class="rounded-xl w-full py-3 px-2 md:px-4 flex justify-between items-center">
       <div class="text-info-3 font-bold text-lg">{{ local.servers }}</div>
       <div class="flex">
         <button
+            @click="updateSubscriptions"
+            class="text-xs md:text-sm mx-2 outline-none border-2 rounded-xl border-success bg-success bg-opacity-20 text-success px-2 md:px-6 py-2 flex space-x-1 items-center">
+          <arrow-path-rounded-square-icon class="w-5 h-5"/>
+          {{ displayHelper(windowWidth).smAndUp ? local.syncSubs : '' }}
+        </button>
+        <button
             @click="updateInbounds"
-            class="mx-2 outline-none border-2 rounded-xl border-success bg-success bg-opacity-20 text-success px-6 py-2 flex space-x-1 items-center text-sm">
+            class="text-xs md:text-sm outline-none border-2 rounded-xl border-success bg-success bg-opacity-20 text-success px-2 md:px-6 py-2 flex space-x-1 items-center">
           <arrow-path-icon class="w-4 h-4"/>
-          {{ local.update }} {{ local.inbounds }}
+          {{ displayHelper(windowWidth).smAndUp ? local.update : '' }}
+          {{ displayHelper(windowWidth).lgAndUp ? local.inbounds : '' }}
         </button>
         <button
             @click="()=>{server = {};showServerDialog = true}"
-            class="outline-none border-2 rounded-xl border-success bg-success bg-opacity-20 text-success px-6 py-2 flex space-x-1 items-center text-sm">
+            class="text-xs md:text-sm mx-2 outline-none border-2 rounded-xl border-success bg-success bg-opacity-20 text-success px-2 md:px-6 py-2 flex space-x-1 items-center">
           <plus-icon class="w-4 h-4"/>
-          {{ local.add }} {{ local.server }}
+          {{ displayHelper(windowWidth).smAndUp ? local.add : '' }}
+          {{ displayHelper(windowWidth).lgAndUp ? local.server : '' }}
         </button>
       </div>
     </div>
-    <servers-list @open-edit-server-dialog="openEditServerDialog" :servers="servers"/>
-    <div class="flex mt-6">
+    <servers-list @open-edit-server-dialog="openEditServerDialog" :servers="servers" :is-loading="loading"/>
+    <div class="flex mt-6" v-if="!loading">
       <div
           class="w-8 h-8 rounded-xl bg-primary-1 bg-opacity-20 flex justify-center items-center mx-1 text-info-3 cursor-pointer transition-all duration-300"
-          v-for="i in pages" :class="{'bg-opacity-50' : onboarding == i}" @click="onboarding = i">{{ i }}
+          v-for="i in pages" :class="{'bg-opacity-50' : onboarding === i}" @click="onboarding = i">{{ i }}
       </div>
     </div>
   </admin-layout>
@@ -42,15 +50,26 @@ import AdminLayout from "../../../layouts/adminLayout.vue";
 import ServersList from "../../../components/serversList.vue";
 import {computed, onMounted, reactive, ref, watch} from "vue";
 import {useLocalization} from "../../../store/localizationStore.js";
-import {PlusIcon, ArrowPathIcon, CheckCircleIcon, XCircleIcon} from "@heroicons/vue/24/solid/index.js";
+import {
+  PlusIcon,
+  ArrowPathIcon,
+  CheckCircleIcon,
+  XCircleIcon,
+  ArrowPathRoundedSquareIcon
+} from "@heroicons/vue/24/outline/index.js";
 import ServerDialog from "../../../components/serverDialog.vue";
 import {useDataStore} from "../../../store/dataStore.js";
 import axios from "axios";
+import Loader from "../../../components/loader.vue";
+import {displayHelper} from "../../../helpers/displayHelper.js";
 
 let local = computed(() => {
   return useLocalization().getLocal
 })
-let pages = ref(10)
+
+let loading = ref(true)
+
+let pages = ref(1)
 let onboarding = ref(1)
 
 let showServerDialog = ref(false)
@@ -62,66 +81,10 @@ const openEditServerDialog = (payload) => {
   showServerDialog.value = true
 }
 
-let servers = ref([
-  {
-    id: '15224',
-    url: 'https://euro-1.gixmetir.online:8090',
-    username: 'husyn.cf',
-    password: '2CsJ7VDd?UtAGm^~!:FDMcd!A^1*epLDP*',
-    generatable: false
-  },
-  {
-    id: '152247',
-    url: 'https://euro-1.gixmetir.online:8090',
-    username: 'husyn.cf',
-    password: '2CsJ7VDd?UtAGm^~!:FDMcd!A^1*epLDP*',
-    generatable: false
-  },
-  {
-    id: '15228',
-    url: 'https://euro-1.gixmetir.online:8090',
-    username: 'husyn.cf',
-    password: '2CsJ7VDd?UtAGm^~!:FDMcd!A^1*epLDP*',
-    generatable: false
-  },
-  {
-    id: '152249',
-    url: 'https://euro-1.gixmetir.online:8090',
-    username: 'husyn.cf',
-    password: '2CsJ7VDd?UtAGm^~!:FDMcd!A^1*epLDP*',
-    generatable: false
-  },
-  {
-    id: '15210',
-    url: 'https://euro-1.gixmetir.online:8090',
-    username: 'husyn.cf',
-    password: '2CsJ7VDd?UtAGm^~!:FDMcd!A^1*epLDP*',
-    generatable: false
-  },
-  {
-    id: '152211',
-    url: 'https://euro-1.gixmetir.online:8090',
-    username: 'husyn.cf',
-    password: '2CsJ7VDd?UtAGm^~!:FDMcd!A^1*epLDP*',
-    generatable: false
-  },
-  {
-    id: '1512',
-    url: 'https://euro-1.gixmetir.online:8090',
-    username: 'husyn.cf',
-    password: '2CsJ7VDd?UtAGm^~!:FDMcd!A^1*epLDP*',
-    generatable: false
-  },
-  {
-    id: '152213',
-    url: 'https://euro-1.gixmetir.online:8090',
-    username: 'husyn.cf',
-    password: '2CsJ7VDd?UtAGm^~!:FDMcd!A^1*epLDP*',
-    generatable: false
-  },
-])
+let servers = ref([])
 
 const loadServers = () => {
+  loading.value = true
   axios.get(`${useDataStore().getServerAddress}/servers/get-all?size=10&page=${onboarding.value - 1}`,
       {
         headers: {
@@ -131,10 +94,17 @@ const loadServers = () => {
   ).then((response) => {
     pages.value = response.data.totalPages
     servers.value = response.data.content
+    loading.value = false
   }).catch((error) => console.log(error))
 }
 
+let windowWidth = ref(0)
+
 onMounted(() => {
+  windowWidth.value = window.innerWidth
+  window.addEventListener('resize', () => {
+    windowWidth.value = window.innerWidth
+  })
   loadServers()
 })
 
@@ -144,9 +114,11 @@ watch(() => onboarding.value, () => {
 
 let showErrorMessage = ref(false)
 let showSuccessMessage = ref(false)
+let errorMessage = ref('')
+let successMessage = ref('')
 
 const updateInbounds = () => {
-  axios.get(`${useDataStore().getServerAddress}/inbounds/load-all-inbounds-from-x-ui-panel`,
+  axios.get(`${useDataStore().getServerAddress}/inbounds/load-all-inbounds-from-x-ui-panels`,
       {
         headers: {
           Authorization: useDataStore().getToken
@@ -154,16 +126,41 @@ const updateInbounds = () => {
       }
   ).then((response) => {
     loadServers()
+    successMessage.value = local.value.inboundsLoadSuccessfully
     showSuccessMessage.value = true
-    setTimeout(()=>{
+    setTimeout(() => {
       showSuccessMessage.value = false
-    } , 2000)
+    }, 2000)
   }).catch((error) => {
     console.log(error)
+    errorMessage.value = local.value.inboundsLoadFailed
     showErrorMessage.value = true
-    setTimeout(()=>{
+    setTimeout(() => {
       showErrorMessage.value = false
-    } , 2000)
+    }, 2000)
+  })
+}
+
+const updateSubscriptions = () => {
+  axios.get(`${useDataStore().getServerAddress}/servers/sync-subscriptions-with-servers`,
+      {
+        headers: {
+          Authorization: useDataStore().getToken
+        }
+      }
+  ).then((response) => {
+    successMessage.value = local.value.subsSyncedSuccessfully
+    showSuccessMessage.value = true
+    setTimeout(() => {
+      showSuccessMessage.value = false
+    }, 2000)
+  }).catch((error) => {
+    console.log(error)
+    errorMessage.value = local.value.syncSubsFailed
+    showErrorMessage.value = true
+    setTimeout(() => {
+      showErrorMessage.value = false
+    }, 2000)
   })
 }
 </script>
