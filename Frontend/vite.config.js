b039d2f6707b33d75d453fab0930439b1b6edfec
mahdi_turrkk <mahdichavoshi14@gmail.com>
2023-09-22 18:52:45 +0330

    pwa added and configs done (not tested)

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,7 +1,49 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+      vue(),
+      VitePWA({
+        registerType : 'autoUpdate',
+        injectRegister : 'auto',
+        workbox : {
+          globPatterns : ['**/*.{js.css.html.ico.png.svg.json.vue.txt.woff2}'],
+          cleanupOutdatedCaches  : true
+        },
+          manifest :{
+            name : 'Management panel for 3X-ui panels',
+              short_name : 'Panel Manager',
+              description : 'Management panel for your 3X-ui panels all in one place.',
+              theme_color : '#ffffff',
+              background_color : '#ffffff',
+              display : 'standalone',
+            start_url : "/",
+              icons : [
+                {
+                  src : '/src/assets/icon-192*192.png',
+                  sizes : '192×192',
+                  type : 'image/png'
+                },
+                {
+                  src : '/src/assets/icon-256*256.png',
+                  sizes : '256×256',
+                  type : 'image/png'
+                },
+                {
+                  src : '/src/assets/icon-384*384.png',
+                  sizes : '384×384',
+                  type : 'image/png'
+                },
+                {
+                  src : '/src/assets/icon-512*512.png',
+                  sizes : '512×512',
+                  type : 'image/png'
+                },
+              ]
+          }
+      })
+  ],
 })
