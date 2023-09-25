aa3ced4e87964d504af20bdfc560c551dfbb219e
hossein <h.shakeri2001@gmail.com>
2023-09-25 20:31:40 +0330

    add break line to configs

--- Backend/src/main/java/online/gixmetir/xuipanelmanagerbackend/services/app/SubscriptionService.java
+++ Backend/src/main/java/online/gixmetir/xuipanelmanagerbackend/services/app/SubscriptionService.java
@@ -179,7 +179,7 @@ public class SubscriptionService {
         List<ClientEntity> entities = clientRepository.findAllBySubscriptionIdAndSendToUser(subscription.getId(), true);
         StringBuilder configs = new StringBuilder();
         for (ClientEntity entity : entities) {
-            configs.append(clientService.generateClientString(entity)).append("\n");
+            configs.append(clientService.generateClientString(entity)).append("<br>");
         }
         return configs.toString();
     }
