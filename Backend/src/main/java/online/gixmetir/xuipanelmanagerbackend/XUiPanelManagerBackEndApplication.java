3744f2f54f890f03deb59689e27e91cfb4eaef3c
hossein <h.shakeri2001@gmail.com>
2023-11-20 00:19:41 +0330

    add custom logger to project

--- Backend/src/main/java/online/gixmetir/xuipanelmanagerbackend/XUiPanelManagerBackEndApplication.java
+++ Backend/src/main/java/online/gixmetir/xuipanelmanagerbackend/XUiPanelManagerBackEndApplication.java
@@ -3,9 +3,11 @@ package online.gixmetir.xuipanelmanagerbackend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableAspectJAutoProxy
 public class XUiPanelManagerBackEndApplication {
 
     public static void main(String[] args) {
