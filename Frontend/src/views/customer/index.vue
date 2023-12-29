74c3faf11b44ceb07bfe1b8efbdbb2821448d5fa
mahdi_turrkk <mahdichavoshi14@gmail.com>
2023-12-30 00:15:45 +0330

    cookie bug fixed

--- Frontend/src/views/customer/index.vue
+++ Frontend/src/views/customer/index.vue
@@ -263,7 +263,7 @@ let isDark = computed(() => {
 const changeThemeStatus = () => {
   showSettingMenu.value = false
   useDataStore().changeDarkStatus()
-  document.cookie = `isDark=${useDataStore().getDarkStatus}`
+  document.cookie = `isDark=${useDataStore().getDarkStatus};path=/`
 }
 
 let showLangMenu = ref(false)
@@ -271,9 +271,9 @@ let customerType = ref("")
 let changeLanguage = (payload) => {
   showLangMenu.value = false
   useLocalization().changeLanguage(payload)
-  document.cookie = `flag=${payload[0]}`
-  document.cookie = `language=${payload[1]}`
-  document.cookie = `direction=${payload[2]}`
+  document.cookie = `flag=${payload[0]};path=/`
+  document.cookie = `language=${payload[1]};path=/`
+  document.cookie = `direction=${payload[2]};path=/`
 }
 
 let local = computed(() => useLocalization().getLocal)
@@ -394,9 +394,9 @@ onMounted(() => {
       } else if (value[0] === 'language') {
         if (value[1].indexOf('[') !== -1) {
           lang = ['🇮🇷', 'fa', 'rtl']
-          document.cookie = `flag=${lang[0]}`
-          document.cookie = `language=${lang[1]}`
-          document.cookie = `direction=${lang[2]}`
+          document.cookie = `flag=${lang[0]};path=/`
+          document.cookie = `language=${lang[1]};path=/`
+          document.cookie = `direction=${lang[2]};path=/`
         } else {
           lang[1] = value[1]
         }
@@ -450,7 +450,7 @@ watch(() => onboarding.value, () => {
 })
 
 const logOut = () => {
-  document.cookie = `token=`
+  document.cookie = `token=;path=/`
   router.push('/')
 }
 
