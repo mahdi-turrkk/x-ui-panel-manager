797977c4d80f98b5e0f275dd7d6fca2e7e98bbf9
mahdi-turrkk <58400554+mahdi-turrkk@users.noreply.github.com>
2024-04-14 01:04:19 +0330

    Update Helper.java

--- Backend/src/main/java/online/gixmetir/xuipanelmanagerbackend/utils/Helper.java
+++ Backend/src/main/java/online/gixmetir/xuipanelmanagerbackend/utils/Helper.java
@@ -31,7 +31,7 @@ public class Helper {
 
     public String generateLink(String uuid) {
 
-        String SUBSCRIPTION_URL = "https://panel.gixmetir.online:5001";
+        String SUBSCRIPTION_URL = "http://panel.privado-vpn.online:5001";
         return SUBSCRIPTION_URL + "/api/v1/subscriptions/client/" + uuid;
     }
 
